c0349b9e5d484919ed01b5267b4df08e2dfe5c09
AKASHGUPTA <akashgupta77500@gmail.com>
2021-08-11 21:46:09 +0530

    payment app

---
+++ base/views.py
@@ -0,0 +1,25 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+import razorpay
+from django.views.decorators.csrf import csrf_exempt
+
+
+def home(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        amount = 50000
+
+        client = razorpay.Client(
+            auth=("rzp_test_bQHBY8SqoVTEvg", "0WO49L6CwklHaS6l17u1G76l"))
+
+        payment = client.order.create({'amount': amount, 'currency': 'INR',
+                                       'payment_capture': '1'})
+    return render(request, 'index.html')
+
+@csrf_exempt
+def success(request):
+    return render(request, "success.html")
+
+
